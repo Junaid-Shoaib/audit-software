770861a2612f4205dc6e4e671613c277264b2857
MuhammadHaris55 <hkhn567@gmail.com>
2022-11-28 15:51:00 +0500

    custom file extension validation and condition when file is approved and reuploading the file with same name

--- resources/js/Pages/Filing/UploadFile.vue
+++ resources/js/Pages/Filing/UploadFile.vue
@@ -2,47 +2,44 @@
   <app-layout>
     <template #header>
       <div class="grid grid-cols-2 items-center">
-        <h2 class="header">
-          Upload File in {{ parent.name }}
-        </h2>
+        <h2 class="header">Upload File in {{ parent.name }}</h2>
       </div>
     </template>
-    <div
-      v-if="$page.props.flash.success"
-      class="bg-green-600 text-white text-center"
-    >
-      {{ $page.props.flash.success }}
-    </div>
-    <div
-      v-if="$page.props.flash.warning"
-      class="bg-yellow-600 contentDSS-center text-white text-center"
-    >
-      {{ $page.props.flash.warning }}
-    </div>
+    <FlashMessage />
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-2">
       <!-- <jet-button @click="create" class="ml-2">Create Account</jet-button> -->
 
       <div class="">
-        <div class="relative  mt-2 ml-2 sm:rounded-2xl">
-             <div
-              class="
-                ml-2
-                bg-red-100
-                border border-red-400
-                text-red-700
-                px-4
-                py-2
-                rounded
-                relative
-              "
-              role="alert"
-              v-if="errors.avatar"
-            >
-              {{ errors.avatar }}
-            </div>
+        <div class="relative mt-2 ml-2 sm:rounded-2xl">
+          <div
+            class="
+              ml-2
+              bg-red-100
+              border border-red-400
+              text-red-700
+              px-4
+              py-2
+              rounded
+              relative
+            "
+            role="alert"
+            v-if="errors.avatar"
+          >
+            {{ errors.avatar }}
+          </div>
           <form @submit.prevent="submit">
             <div class="p-8 -mr-6 -mb-8 flex flex-wrap">
-              <input class="border-solid border-2 border-indigo-300 rounded-xl px-6 py-2 m-4" type="file" v-on:change="onFileChange" />
+              <input
+                class="
+                  border-solid border-2 border-indigo-300
+                  rounded-xl
+                  px-6
+                  py-2
+                  m-4
+                "
+                type="file"
+                v-on:change="onFileChange"
+              />
               <!-- <progress
                 v-if="form2.progress"
                 :value="form2.progress.percentage"
@@ -50,12 +47,7 @@
               >
                 {{ form2.progress.percentage }}%
               </progress> -->
-              <button
-                class="trailbutton"
-                type="submit"
-              >
-                Upload File
-              </button>
+              <button class="trailbutton" type="submit">Upload File</button>
             </div>
           </form>
         </div>
@@ -67,6 +59,7 @@
 
 <script>
 import AppLayout from "@/Layouts/AppLayout";
+import FlashMessage from "@/Layouts/FlashMessage";
 import JetButton from "@/Jetstream/Button";
 import Paginator from "@/Layouts/Paginator";
 import { pickBy } from "lodash";
@@ -84,11 +77,12 @@ export default {
 
   components: {
     AppLayout,
+    FlashMessage,
     // Treeselect,
   },
 
   props: {
-    errors:Object,
+    errors: Object,
     data: Object,
     fold: Object,
     parent: Object,
